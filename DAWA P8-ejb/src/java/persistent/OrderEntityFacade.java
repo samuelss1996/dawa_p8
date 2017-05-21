fbd0cf806f552c6eaabd9b19e3f0f599ff779ea7
Unknown <samuelsoutullosobral@gmail.com>
2017-05-21 18:06:54 +0200

    Added UML file

--- DAWA P8-ejb/src/java/persistent/OrderEntityFacade.java
+++ DAWA P8-ejb/src/java/persistent/OrderEntityFacade.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package persistent;
 
 import javax.ejb.Stateless;
@@ -10,10 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import persistent.entities.OrderEntity;
 
-/**
- *
- * @author Ada
- */
 @Stateless
 public class OrderEntityFacade implements OrderEntityFacadeLocal, OrderEntityFacadeRemote {
     @PersistenceContext(unitName = "DAWA_P8-ejbPU")
