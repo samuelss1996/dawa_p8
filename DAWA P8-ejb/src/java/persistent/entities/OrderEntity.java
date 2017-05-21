5eabce87667358bcdf9b8fe42bb0daf2f3c76d5c
Unknown <samuelsoutullosobral@gmail.com>
2017-05-21 13:54:17 +0200

    Working database tables generation

--- DAWA P8-ejb/src/java/persistent/entities/OrderEntity.java
+++ DAWA P8-ejb/src/java/persistent/entities/OrderEntity.java
@@ -34,7 +34,7 @@ public class OrderEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
